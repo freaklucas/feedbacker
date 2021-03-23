fb97a8c1638e0b031892c61285adcc9abe1cfdb8
freaklucas <lucas.oliveira.freak@gmail.com>
2021-03-22 21:38:17 -0300

    configurando cor dos filtros

--- dashboard/src/views/Feedbacks/Filters.vue
+++ dashboard/src/views/Feedbacks/Filters.vue
@@ -1,33 +1,34 @@
+
 <template>
-    <div class="flex flex-col">
-        <h1 class="text-2xl font-regular text-brand-darkgray">
-            Filtros
-        </h1>
-        <ul class="flex flex-col mt-3 list-none">
-            <li
-            v-for="filter in state.filters"
-            :key="filter.label"
-            :class="{
-                'bg-gray-200 bg-opacity-50': filter.active
-            }"
-            @click="() => handleSelect(filter)"
-            class="flex items-center justify-between px-4 py-1 rounded cursor-pointer"
-            >
-            <div class="flex items-center">
-                <span
-                 :class="filters.color.bg"
-                 class="inline-block w-2 h-2 mr-2 rounded-full"/>
-                 {{ filter.label }}
-            </div>
-            <span
-                :class="filter.active ? filter.color.text : 'text-brand-graydark'"
-                class="font-bold"
-            >
-                {{ filter.amount }}
-            </span>
-            </li>
-        </ul>
-    </div>
+  <div class="flex flex-col">
+    <h1 class="text-2xl font-regular text-brand-darkgray">
+      Filtros
+    </h1>
+
+    <ul class="flex flex-col mt-3 list-none">
+      <li
+        v-for="filter in state.filters"
+        :key="filter.label"
+        :class="{
+          'bg-gray-200 bg-opacity-50': filter.active
+        }"
+        @click="() => handleSelect(filter)"
+        class="flex items-center justify-between px-4 py-1 rounded cursor-pointer"
+      >
+        <div class="flex items-center">
+          <span
+            :class="filter.color.bg"
+            class="inline-block w-2 h-2 mr-2 rounded-full"/> {{ filter.label }}
+        </div>
+        <span
+          :class="filter.active ? filter.color.text : 'text-brand-graydark'"
+          class="font-bold"
+        >
+          {{ filter.amount }}
+        </span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
