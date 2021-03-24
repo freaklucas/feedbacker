bc7f03924751f4b45b1ada11a868073ede032897
freaklucas <lucas.oliveira.freak@gmail.com>
2021-03-24 01:30:39 -0300

    setup inicial widget

---
+++ widget/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <widget />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Widget from '../src/views/Widget/index.vue'
+
+export default defineComponent({
+  components: {
+    Widget
+  }
+})
+</script>
