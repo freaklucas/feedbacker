c70e6a878ba511de2962d8f897c0c9f6798dfac4
freaklucas <lucas.oliveira.freak@gmail.com>
2021-03-24 22:03:42 -0300

    criando hooks, box e standby

---
+++ widget/src/views/Widget/Standby.vue
@@ -0,0 +1,29 @@
+<template>
+    <div
+        click="() => emit('open-box')"
+        class="fixed z-50 bottom-0 right-0 mb-5 mr-5 bg-brand-main rounded-full
+            py-3 px-5 flex items-center shadow-xl cursor-pointer select-none
+            animate__animated animate__fadeInUp animate__faster
+    ">
+
+    <span class="font-black text-white text-xl">
+        Deixe um feedback
+    </span>
+
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, SetupContext } from 'vue'
+interface SetupReturn {
+    emit: SetupContext['emit'];
+}
+
+export default defineComponent {
+    setup(_,{ emit }: SetupContext): SetupReturn {
+        return {
+            emit
+        }
+    }
+}
+</script>
